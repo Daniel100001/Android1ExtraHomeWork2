c95b97d8c4337248547d33051e493adc2e8ccc82
Daniel100001 <danldplayer2gmail.com>
2023-04-18 16:01:39 +0600

    It's Android 2Home Work 2 - full

--- app/src/main/java/com/example/android1extrahomework2/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/android1extrahomework2/ui/activity/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.android1extrahomework2.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.android1extrahomework2.R
+import com.example.android1extrahomework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
